47e9c2aa78980b0ad7709defe048e3894ef52ed1
NAHLA KHALIFA <nahlaf54@gmail.com>
2023-07-24 22:30:20 -0500

    banner adedd

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,10 +1,28 @@
-const Buttons = () => {
+import satData from "./satData"
+
+const Buttons = ({ filterByType, setSat, displaySats }) => {
+    const satData = []; 
+
   return (
+  <div>
+    
     <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
-      </div>
+  {displaySats.map((sat, id) => {
+  return (
+    <button onClick={() => filterByType(sat)} key={id}>
+      {sat} Orbit
+    </button>
   );
-};
+})}  
+    </div>
+    
+
+    <button onClick={() =>setSat(satData)}> All orbits
+    </button>
+    
+  
+  </div>
+  
+  )};
 
 export default Buttons;
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,15 +1,25 @@
 const Table = ({ sat }) => {
+  const getStatus = (satellite) => {
+    return satellite.operational ? 'Active' : 'Inactive';
+  };
   return (
       <table>
        <thead>
         <tr>
-          <th>Header TBD</th>
+          <th>Name</th>
+          <th>Type of Satellite</th>
+          <th>Launch Date</th>
+          <th>Status</th>
         </tr>
         </thead>
-        <tbody>
-        <tr>
-          <td>Row Data TBD</td>
+        <tbody>{sat.map((data) => (
+        <tr key={data.id}>
+          <td>data.name</td>
+          <td>data.type</td>
+          <td>data.launchDate</td>
+          <td>data.Status</td>
         </tr>
+        ))};
         </tbody>
       </table>
   );
